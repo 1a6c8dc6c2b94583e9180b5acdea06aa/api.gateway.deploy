538253ee9308375d161cbb1cf681be542908b378
System <Mr.NN91@gmail.com>
2016-12-21 15:13:44 +0700

    Update portal-api.py

--- docs/portal-api.py
+++ docs/portal-api.py
@@ -2,4 +2,25 @@
 
 # Environment Variables available
 
+# http://wickedhaufeio.readthedocs.io/en/latest/docker-images/
+
+
+# portal-api:
+
+NODE_ENV
+
+PORTAL_CONFIG_KEY
+
+
+
+
+
+
+
+
+
+
+
+
+
 
